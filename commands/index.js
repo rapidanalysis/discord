59b66db2a8791e2d2181d0de0ef16eaacaba6eb4
crpz1 <8588315+crpz1@users.noreply.github.com>
2024-05-16 16:04:12 +1000

    base: assume we need to check registration

--- commands/index.js
+++ commands/index.js
@@ -29,14 +29,17 @@ class CommandManager {
 class BaseCommand {
     name = "basecommand";
     data = new SlashCommandBuilder();
+    #connection;
 
     /**
      * Construct a new Command.
      * @param {SlashCommandBuilder} command - The command.
+     * @param {import("mysql2/promise").Connection} connection - The database connetion
      */
-    constructor(command) {
+    constructor(command, connection) {
         this.data = command;
         this.name = command.name;
+        this.#connection = connection;
     }
 
     /**
@@ -44,7 +47,16 @@ class BaseCommand {
      * @param {BaseInteraction} interaction - The interaction that comes from Discord.
      */
     async execute(interaction) {
-        await interaction.reply("test");
+        // Check if the user is registered
+        console.log('Checking if user is registered');
+        const [rows] = await this.#connection.execute('SELECT apikey, percent, privacy, limitc FROM user_profile WHERE uid = ?', [userId]);
+
+        if (rows.length === 0) {
+            // User is not registered
+            await interaction.reply('You are not registered!\nPlease go to https://rapidanalysis.github.io/getting-started.html to register and get api key\nThen enter /reg + [api key]');
+            return false;
+        }
+        return true;
     }
 }
 
